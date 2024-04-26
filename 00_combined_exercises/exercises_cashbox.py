12f954862e7578486cc9087554ba0c92ade92792
hoomanshz <tayebi.hooman@gmail.com>
2024-04-26 12:24:36 +0200

    V0.2

--- 00_combined_exercises/exercises_cashbox.py
+++ 00_combined_exercises/exercises_cashbox.py
@@ -1,12 +1,18 @@
 while True:
-    amount_to_be_paid = int(input("Enter the amount to be paid: "))
-    amount_received = int(input("Enter the amount received: "))
 
-    if amount_to_be_paid < 0 or amount_received < 0:
-        print("Invalid input. Amounts cannot be negative.")
-    elif amount_received < amount_to_be_paid:
+    to_be_paid = int(input("Enter the amount to be paid: "))
+    if to_be_paid <= 0:
+        print("Invalid input. Amounts cannot be negative or zero.")
+    
+    amount_received = int(input("Enter the amount received: "))
+    if amount_received <= 0:
+        print("Invalid input. Amounts cannot be negative or zero.")
+    
+    elif amount_received < to_be_paid:
         print("Amount received is too small.")
+    
     else:
-        change = amount_received - amount_to_be_paid
-        print("Change:", change)
+        change = amount_received - to_be_paid
+        #print("Change:", change)
+        print(f"Change: {change}")
         break
